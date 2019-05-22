24977eec700f4efa38ff9e1d32d027b9ebb780c2
irina_petrushenko <irina.aninger@gmail.com>
2019-05-22 17:36:12 +0300

    123

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 var gulp         = require('gulp'), // Подключаем Gulp
     sass         = require('gulp-sass'), //Подключаем Sass пакет,
     browserSync  = require('browser-sync'), // Подключаем Browser Sync
-    concat       = require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов)
-    uglify       = require('gulp-uglifyjs'), // Подключаем gulp-uglifyjs (для сжатия JS)
+    concat       = require('gulp-concat'), // Подключаем pug-concat (для конкатенации файлов)
+    uglify       = require('gulp-uglifyjs'), // Подключаем pug-uglifyjs (для сжатия JS)
     cssnano      = require('gulp-cssnano'), // Подключаем пакет для минификации CSS
     rename       = require('gulp-rename'), // Подключаем библиотеку для переименования файлов
     del          = require('del'), // Подключаем библиотеку для удаления файлов и папок
@@ -12,22 +12,53 @@ var gulp         = require('gulp'), // Подключаем Gulp
     autoprefixer = require('gulp-autoprefixer');// Подключаем библиотеку для автоматического добавления префиксов
     uncss        = require('gulp-uncss');
     pug          = require('gulp-pug');
+    htmlbeautify = require('gulp-html-beautify');
+    notify = require("gulp-notify");
+    plumber = require('gulp-plumber');
+
+
+gulp.src('app/*.ext')
+    .pipe(plumber())
+    .pipe(gulp.dest('./dist'));
+gulp.src("app/test.ext")
+    .pipe(notify("Found file: <%= file.relative %>!"));
 
 gulp.task('sass', function() { // Создаем таск Sass
     return gulp.src('app/sass/**/*.sass') // Берем источник
-        .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+        .pipe(sass()) // Преобразуем Sass в CSS посредством pug-sass
         .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
         .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
         .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
 });
 
-gulp.task('pug', function() { // Создаем таск Pug
-    return gulp.src('app/pug/pages/*.pug') // Берем источник
+gulp.task('htmlbeautify', function() {
+    var options = {
+    indentSize: 2,
+        unformatted: [
+            // https://www.w3.org/TR/html5/dom.html#phrasing-content
+            'abbr', 'area', 'b', 'bdi', 'bdo', 'br', 'cite',
+            'code', 'data', 'datalist', 'del', 'dfn', 'em', 'embed', 'i', 'ins', 'kbd', 'keygen', 'map', 'mark', 'math', 'meter', 'noscript',
+            'object', 'output', 'progress', 'q', 'ruby', 's', 'samp', 'small',
+            'strong', 'sub', 'sup', 'template', 'time', 'u', 'var', 'wbr', 'text',
+            'acronym', 'address', 'big', 'dt', 'ins', 'strike', 'tt'
+        ]
+};
+    gulp.src('./*.html')
+        .pipe(htmlbeautify(options))
+        .pipe(gulp.dest('app'))
+});
+
+gulp.task('pug', function() {
+    return gulp.src("app/pug/pages/*.pug")
+        .pipe(plumber({
+            errorHandler: notify.onError()
+        }))
         .pipe(pug({
-            pretty:true
-        })) // Преобразуем Pug в HTML посредством gulp-pug
-        .pipe(gulp.dest('app/html')) // Выгружаем результата в папку app
-        .pipe(browserSync.reload({stream: true}))
+            pretty: true
+        }))
+        .pipe(gulp.dest("app"))
+        .pipe(htmlbeautify())
+        .pipe(browserSync.stream());
 });
 
 gulp.task('browser-sync', function() { // Создаем таск browser-sync
