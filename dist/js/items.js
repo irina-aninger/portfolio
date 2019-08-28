555ff223c1c3b5e5b174ea5a1560aabccb9f28b6
irina_petrushenko <irina.aninger@gmail.com>
2019-08-28 21:45:52 +0300

    big part

---
+++ dist/js/items.js
@@ -0,0 +1,261 @@
+let dataItem;
+dataItem = {
+    'co-working': {
+        'title': 'Co-working',
+        'link': 'https://irina-aninger.github.io/together/',
+        'desc': 'Landing page was performed using Gulp. For styles used SASS. Animation is done with wow.js and animate.css',
+        'tags': ['HTML', 'CSS', 'SASS', 'Gulp', 'JQuery'],
+        'image': './img/work/1_front.png'
+    },
+    'kinopoisk': {
+        'title': 'Kinopoisk',
+        'link': 'https://irina-aninger.github.io/movies/',
+        'desc': 'The project is executed using TMDb API and JavaScript. Bootstrap and CSS3 were used in the development of the layout.',
+        'tags': ['HTML', 'CSS', 'JS', 'API', 'Bootstrap'],
+        'image': './img/work/2_front.png'
+    },
+    'garantiruem': {
+        'title': 'garantiruem',
+        'link': 'https://garantiruem.by/',
+        'desc': `Development of the following elements:
+        <ul>
+            <li>Top dropdown menu</li>
+            <li>Fixing top menu</li>
+            <li><a href="https://garantiruem.by/#lease">Catalog of real estate Agency</a> on the main page</li>
+            <li>News section on the main page</li>
+            <li><a href="https://garantiruem.by/articles/">The template for the page articles</a>, the upper filter subscription</li>
+            <li>The conclusion of the subscription and other articles for <a href="https://garantiruem.by/articles/novosti-kompanii/sovmestnoe-vremyaprovozhdenie-pozvolyaet-podderzhivat-khoroshie-otnosheniya-v-kollektive/">the pages of individual articles</a></li>
+        </ul>
+        `,
+        'tags': ['HTML', 'CSS', 'JQuery', 'JS', 'Bitrix', 'Bootstrap'],
+        'image': './img/work/3_front.png'
+    },
+    'gradient': {
+        'title': 'Gradient',
+        'link': 'https://www.gradient.by/',
+        'desc': `Development of the following elements:
+        <ul>
+            <li>All feedback forms on the site with validation</li>
+            <li>Through section "Our advantages"</li>
+            <li>Through section "Trust us"</li>
+            <li>Accordion on <a href="https://www.gradient.by/vopros-otvet.html">the FAQ page</a></li>
+            <li>Integration of these elements with CMS</li>
+        </ul>
+        `,
+        'tags': ['HTML', 'CSS', 'JQuery', 'JS', 'MODX'],
+        'image': './img/work/4_front.png'
+    },
+    'belkontplus': {
+        'title': 'Belkontplus',
+        'link': 'http://www.belkontplus.by/',
+        'desc': `Development of the following elements:
+        <ul>
+            <li>Slider with Slick Slider on the main page</li>
+            <li>Validation for feedback forms</li>
+            <li>Edit incorrect adaptive</li>
+        </ul>
+        `,
+        'tags': ['HTML', 'CSS', 'JQuery', 'JS', 'Slick Slider', 'Joomla'],
+        'image': './img/work/5_front.png'
+    },
+    'eurokupe': {
+        'title': 'Eurokupe',
+        'link': 'http://eurokupe.by/',
+        'desc': `Development of the following elements:
+        <ul>
+            <li>A complete redesign</li>
+            <li>Section "News from Instagram"</li>
+            <li>Partial adaptive</li>
+        </ul>
+        `,
+        'tags': ['HTML', 'CSS', 'JQuery', 'Joomla'],
+        'image': './img/work/6_front.png'
+    },
+    '20dc': {
+        'title': '20dc',
+        'link': 'https://20dc.by/',
+        'desc': `Development of the following elements:
+        <ul>
+            <li>Top dropdown menu</li>
+            <li>Adapting header with a dropdown menu</li>
+            <li>Section "Our advantages" on the main page</li>
+        </ul>
+        `,
+        'tags': ['HTML', 'CSS', 'JQuery', 'JS', 'WordPress'],
+        'image': './img/work/8_front.png'
+    },
+    'ecosoft': {
+        'title': 'Ecosoft',
+        'link': 'https://ecosoft.by/',
+        'desc': `Development of the following elements:
+        <ul>
+            <li>Top dropdown menu</li>
+            <li>Section "Best deal" on the main page</li>
+            <li>Section "Our advantages" on the main page</li>
+            <li>Section "Request a call professional's" on the main page with modal window</li>
+            <li>The map in the footer</li>
+            <li>Section "Catalog" on <a href="https://ecosoft.by/catalog/">the catalog page</a></li>
+            <li>Form to buy in installments of the goods</li>
+            <li>Full adaptive of the site</li>
+        </ul>
+        `,
+        'tags': ['HTML', 'CSS', 'JQuery', 'JS', 'MODX'],
+        'image': './img/work/15_front.png'
+    },
+    'ecodar': {
+        'title': 'Ecodar',
+        'link': 'https://ecodar.by/',
+        'desc': `Development of the following elements:
+        <ul>
+            <li>Top dropdown menu</li>
+            <li>Section "List of funeral services" on the main page</li>
+            <li>Section "Advantages of cooperation with us" on the main page</li>
+            <li>Section with categories on all pages of the site</li>
+            <li>Page <a href="https://ecodar.by/galereya-fotografij/">"Gallery shop"</a></li>
+            <li>Left side dropdown menu in categories</a></li>
+        </ul>
+        `,
+        'tags': ['HTML', 'CSS', 'JQuery', 'JS', 'WordPress'],
+        'image': './img/work/16_front.png'
+    },
+    'elegiadent': {
+        'title': 'Elegiadent',
+        'link': 'https://elegiadent.by/',
+        'desc': `Development of the following elements:
+        <ul>
+            <li>Top dropdown menu</li>
+            <li>Adaptive mobile menu</li>
+        </ul>
+        `,
+        'tags': ['HTML', 'CSS', 'JQuery', 'JS', 'Bitrix'],
+        'image': './img/work/17_front.png'
+    },
+    'florita': {
+        'title': 'Florita',
+        'link': 'https://florita.by/',
+        'desc': `Development of the following elements:
+        <ul>
+            <li>Sliders with products on the main page</li>
+            <li>Page <a href="https://florita.by/katalog/nedavnie-bukety/">"Our works"</a></li>
+            <li>Filter on catalog's pages</li>
+            <li>Left-Side dropdown menu on catalog's pages</li>
+            <li><a href="https://florita.by/katalog/bukety/rozy/rozovye/buket-iz-belo-rozovyix-roz/">Product pages</a></li>
+            <li>Adaptive filter, catalog, product pages, sliders, footer</li>
+            <li>Integration of these elements with CMS</li>
+        </ul>
+        `,
+        'tags': ['HTML', 'CSS', 'JQuery', 'JS', 'MODX'],
+        'image': './img/work/18_front.png'
+    },
+    'gold': {
+        'title': 'Золотая подкова',
+        'link': 'https://xn--80aafhnuexbbci1b1n.xn--90ais/',
+        'desc': `Development of the following elements:
+        <ul>
+            <li>Automatic output of events to the section "Upcoming events"</li>
+            <li>Automatic output of events to the section "Other events"</li>
+            <li>Automatic output of events to the section "Photo Reports of events"</li>
+            <li>Automatic output of events to the section "Photos of other events"</li>
+            <li>Adaptive header</li>
+        </ul>
+        `,
+        'tags': ['HTML', 'CSS', 'JQuery', 'JS', 'MODX'],
+        'image': './img/work/20_front.png'
+    },
+    'wealth': {
+        'title': 'Wealth',
+        'link': 'https://wealth.by/',
+        'desc': `Development of the following elements:
+        <ul>
+            <li>Dropdown menu on the main page and on the category pages</li>
+            <li>General page <a href="https://wealth.by/dexen/">with products</a></li>
+            <li><a href="https://wealth.by/dexen/">Product page</a></li>
+        </ul>
+        `,
+        'tags': ['HTML', 'CSS', 'JQuery', 'JS', 'MODX'],
+        'image': './img/work/21_front.png'
+    },
+    'transfer-minsk': {
+        'title': 'Transfer Minsk',
+        'link': 'https://transfer-minsk.com/',
+        'desc': `Development of the following elements:
+        <ul>
+            <li>Page <a href="https://transfer-minsk.com/poleznaya-informacziya/sanatorii/">"Sanatorium"</a> and page <a href="https://transfer-minsk.com/poleznaya-informacziya/gostiniczy/">"Hotels"</a></li>
+            <li><a href="https://transfer-minsk.com/poleznaya-informacziya/sanatorii/im.-orlovskogo.html">Object detail page</a></li>
+            <li>Integration these pages with CMS</li>
+        </ul>
+        `,
+        'tags': ['HTML', 'CSS', 'MODX'],
+        'image': './img/work/22_front.png'
+    },
+    'rcompany': {
+        'title': 'Rcompany',
+        'link': 'https://rcompany.by/',
+        'desc': `Development of the following elements:
+        <ul>
+            <li>Section "Our advantage"</li>
+            <li>Feedback form on the page <a href="https://rcompany.by/page/tseny">"Prices"</a> and <a href="https://rcompany.by/uslugi/auditorskie-uslugi">"Audit services"</a></li>
+        </ul>
+        `,
+        'tags': ['HTML', 'CSS', 'JQuery', 'JS'],
+        'image': './img/work/24_front.png'
+    },
+    'homsair': {
+        'title': 'Homsair',
+        'link': 'http://homsair.vldpl.me/',
+        'desc': `Development of the following elements:
+        <ul>
+            <li>Imposing a layout on <a href="http://homsair.vldpl.me/katalog/duhovye-shkafy/">a standard filter Bitrix</a></li>
+        </ul>
+        `,
+        'tags': ['HTML', 'CSS', 'JQuery', 'JS', 'PHP', 'Bitrix'],
+        'image': './img/work/31_front.png'
+    },
+    'anbosta': {
+        'title': 'Anbosta',
+        'link': 'https://www.anbosta.by/',
+        'desc': `Development of the following elements:
+        <ul>
+            <li>Full adaptive of the site</li>
+            <li>Adaptive of <a href="https://www.anbosta.by/searchtours">third-party iframes</a></li>
+        </ul>
+        `,
+        'tags': ['HTML', 'CSS', 'JQuery', 'JS', 'Joomla'],
+        'image': './img/work/32_front.png'
+    },
+    'hmelnov': {
+        'title': 'Hmelnov',
+        'link': 'https://hmelnov.by/rating/',
+        'desc': `Development of the following elements:
+        <ul>
+            <li>Page <a href="https://hmelnov.by/rating/">"Rating"</a></li>
+        </ul>
+        `,
+        'tags': ['HTML', 'CSS', 'JS', 'Joomla'],
+        'image': './img/work/33_front.png'
+    },
+    'inoxtrade': {
+        'title': 'Inoxtrade',
+        'link': 'https://inoxtrade.by/',
+        'desc': `Development of the following elements:
+        <ul>
+            <li><a href="https://inoxtrade.by/#categories-block">Section with categories</a> on the main page</li>
+            <li><a href="https://inoxtrade.by/catalog/konditerskoe-xlebopekarnoe-oborudovanie/">Category pages</a></li>
+            <li><a href="https://inoxtrade.by/catalog/konditerskoe/">Subcategory pages</a></li>
+        </ul>
+        `,
+        'tags': ['HTML', 'CSS', 'JS', 'Joomla'],
+        'image': './img/work/34_front.png'
+    },
+    'ph2': {
+        'title': 'Ph2',
+        'link': 'http://ph2.pwd.by/',
+        'desc': `Development of the following elements:
+        <ul>
+            <li>Imposing a layout on <a href="http://ph2.pwd.by/catalog/bolty/">a standard filter Bitrix</a></li>
+        </ul>
+        `,
+        'tags': ['HTML', 'CSS', 'JQuery', 'JS', 'PHP', 'Bitrix'],
+        'image': './img/work/35_front.png'
+    }
+};
